f2664b6c05f32b5ff0545e8f4f9d26ef555bdffd
EddyGharbi <eddygharbiema@gmail.com>
2021-01-22 13:15:54 -0800

    Fix windows build issue

--- headers/EditorWidget.h
+++ headers/EditorWidget.h
@@ -42,6 +42,20 @@ struct sync_data {
 	bool                    ran = false;
 };
 
+#ifdef WIN32
+
+enum WM_USER_MSG {
+	// Start at index user + 5 because some plugins were causing issues when sending invalid
+	// messages to the main window
+	WM_USER_SET_TITLE = WM_USER + 5,
+	WM_USER_SHOW,
+	WM_USER_CLOSE,
+	WM_USER_LOAD_DLL,
+	WM_USER_SETCHUNK
+};
+
+#endif
+
 class VSTPlugin;
 
 class VstRect {
