b4a73d6ea81dbade0775762b63616d6193fa9259
unknown <stevensoftware52@gmail.com>
2022-03-11 11:45:40 -0800

    Revise startup for process to use module binary path
    Working directory is unreliable

--- win/VSTPlugin-win.cpp
+++ win/VSTPlugin-win.cpp
@@ -48,7 +48,10 @@ AEffect* VSTPlugin::loadEffect()
 	m_effect = std::make_unique<AEffect>();
 	std::wstring startparams = L"streamlabs_vst.exe \"" + std::wstring(wpath) + L"\" " + std::to_wstring(portNumber) + L" " + std::to_wstring(GetCurrentProcessId());
 
-	if (!CreateProcessW(L"win-streamlabs-vst.exe", (LPWSTR)startparams.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &m_winServer))
+	std::string module_path = obs_get_module_binary_path(obs_get_module("obs-vst"));
+	std::wstring process_path = std::filesystem::path(module_path).remove_filename().wstring() + L"/win-streamlabs-vst.exe";
+
+	if (!CreateProcessW(process_path.c_str(), (LPWSTR)startparams.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &m_winServer))
 	{
 		::MessageBoxA(NULL, (std::filesystem::path(m_pluginPath).filename().string() + " failed to launch.\n\n You may restart the application or recreate the filter to try again.").c_str(), "VST Filter Error",
 			MB_ICONERROR | MB_TOPMOST);
