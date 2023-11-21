40d8e82217febf1dd3bd934ad4c1420d8940c05c
Vladimir Sumarov <summeroff@gmail.com>
2023-11-21 12:39:35 -0800

    codestyle fix

--- headers/VSTPlugin.h
+++ headers/VSTPlugin.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BLOCK_SIZE 512
 
 #ifdef WIN32
-	#define NOMINMAX
-	#include <Windows.h>
+#define NOMINMAX
+#include <Windows.h>
 #endif
 
 #include <string>
